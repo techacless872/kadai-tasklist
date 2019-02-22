8e2ddcf8cb1505ec2b73c3c72ef004b37a02fa84
techacless872 <techacless872@gmail.com>
2019-02-22 11:17:33 +0000

    tasklist login kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,26 @@
 class TasksController < ApplicationController
-  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.all.page(params[:page]).per(25)
+      # @tasks = Task.all.page(params[:page]).per(25)
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page]).per(25)
   end
 
   def show
   end
 
   def new
-      @task = Task.new
+      # @task = Task.new
+      # @task.user = current_user
+      
+      @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    # @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
@@ -56,6 +62,13 @@ class TasksController < ApplicationController
 # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
